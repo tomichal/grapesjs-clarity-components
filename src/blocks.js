4a5f60415eff047da062afa74e1de2b9f77498aa
Michal Tomaszewski <michalt48@gmail.com>
2017-11-02 13:45:37 -0700

    wip - add clarity components

--- src/blocks.js
+++ src/blocks.js
@@ -1,8 +1,8 @@
 export default (editor, config = {}) => {
   const blockManager = editor.BlockManager;
 
-  blockManager.add('clarity-btn', {
-    label: 'Button',
+  blockManager.add('clarity-button', {
+    label: "Button",
     content: '<a class="btn btn-primary">Button</a>',
   });
 }
--- src/components.js
+++ src/components.js
@@ -1,7 +1,52 @@
 export default (editor, config = {}) => {
-  const domc = editor.DomComponents;
-  const defaultType = domc.getType('default');
-  const defaultModel = defaultType.model;
-  const defaultView = defaultType.view;
-  // ...
+  const domComponents = editor.DomComponents;
+  const type = domComponents.getType('default');
+  const model = type.model;
+  const view = type.view;
+
+  domComponents.addType('clarity-button', {
+    model: model.extend({
+        init() {
+          this.listenTo(this, 'change:button-type', this.changeButtonType);
+        },
+        changeButtonType(model, type) {
+          this.view.$el.removeClass('btn, btn-primary, btn-link')
+          this.view.$el.addClass(type)
+        },
+        defaults: Object.assign({}, model.prototype.defaults, {
+          draggable: '*',
+          droppable: false,
+          label: 'Button',
+          traits: [{
+            type: 'select',
+            label: 'Type',
+            name: 'button-type',
+            changeProp: 1,
+            options: [
+              {value: 'btn btn-primary', name: 'Primary'},
+              {value: 'btn btn-link', name: 'Link'}
+            ]
+          }]
+        })
+      },
+      {
+        isComponent: function (el) {
+          if ($(el).hasClass('btn')) {
+            return { type: 'clarity-button' };
+          }
+        }
+      }),
+
+    view: type.view
+    
+    // view: view.extend({
+    //   init() {
+    //     this.listenTo(this, 'change:button-type', this.changeButtonType);
+    //     console.log(this)
+    //   },
+    //   changeButtonType() {
+    //     alert(this)
+    //   }
+    // })
+  })
 }
